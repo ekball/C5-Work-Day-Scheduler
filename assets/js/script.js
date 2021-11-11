a0a01019483dfec71ee702a0e6cb736ae3cfba0b
ekball <90882418+ekball@users.noreply.github.com>
2021-11-10 22:25:49 -0500

    removed all superfluous (failed) code relating to dynamic text input field

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,56 +46,9 @@ var createText = function () {
         $(this).replaceWith(newEl);
 
     });
-
-    // // create a text area
-    // var inputText = $("<textarea/>")
-
-    // // create a variable to hold the content of the original element's text
-    // var inputContainerEl = $("#text-container").text();
-
-    // // put the contents of the original element into the new text area so it can be edited
-    // inputText.text(inputContainerEl);
-
-    // // replace the original text container with the inputText textarea
-    // $("#text-container").replaceWith(inputText);
-
-
     
 };
 
-// create field to type in when the area is clicked on   
-var createTextboxHandler = function (event) {
-
-    // var inputText = $("<textarea/>")
-
-    // // save the text in the input container
-    // var inputContainerEl = $(this).text();
-
-    // inputText.text(inputContainerEl);
-    // // replace the text container with the inputText textarea
-    // $("focusOn").replaceWith(inputText);
-
-    // // puts focus on the text area
-    // inputText.focus();
-
-};
-
-// remove field to type in when the area is out of focus
-var removeTextboxHandler = function () {
-
-        // // save the text in the text area that was just clicked on and used
-        // var oldTextContent = $(this).text();
-
-        // // create a new container to swap out with the text area
-        // var newTextContent = $("<div>");
-
-        // // put in the new value, which was the old content
-        // newTextContent.text(oldTextContent);
-
-        // // swap the elements from text area to div
-        // $(this).replaceWith(newTextContent);
-
-};
 
 var main = function () {
 
@@ -105,15 +58,3 @@ var main = function () {
 };
 
 main();
-
-
-
-
-
-
-// event handler for clicking on the center column - puts focus on the text area
-$("#text-container").click(createTextboxHandler());
-
-
-// event handler for clicking off of the center column - pulls the text area out of focus
-$("#text-container").blur(removeTextboxHandler());
