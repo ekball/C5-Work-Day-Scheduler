20cd4b12dbdfd9129a6e3b026e8a3c4324c3d0f8
ekball <90882418+ekball@users.noreply.github.com>
2021-11-11 22:55:24 -0500

    fixed/added a dynamic text input field for the middle column and began coding the background changing mechanic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var dayEl = document.querySelector("#currentDay")
 var inputContainerEl = document.querySelector(".text-box");
 
+
+var workDayHours = [];
+
 var displayCurrentDay = function () {
     
     dayEl.textContent = moment().format('MMM Do YYYY');
@@ -9,52 +12,49 @@ var displayCurrentDay = function () {
 
 var createText = function () {
 
-    var inputAreaEl = document.createElement("p");
-    var inputEl = document.createElement("textarea");
+    // give all elements with class 'text-box' a contenteditable value of true
+        // this creates a dynamic user input box that blurs when clicked out of
 
-    inputAreaEl.appendChild(inputEl);
-    inputContainerEl.appendChild(inputAreaEl);
-    
-    $("#text-container0 ,#text-container1, #text-container2, #text-container3, #text-container4, #text-container5, #text-container6, #text-container7, #text-container8").on("click", function() {
+    $(".text-box").attr("contenteditable", "true");
 
-        var text = $(this)
-            .text()
-            .trim();
-
-        var textInput = $("<textarea>")
-            .addClass("form-control")
-            .val(text);
-
-        textInput.trigger("focus");
-
-        $(this).replaceWith(textInput);
-        
-    });
-
-    $("#text-container0,#text-container1, #text-container2, #text-container3, #text-container4, #text-container5, #text-container6, #text-container7, #text-container8").on("blur", "textarea", function () {
-
-        // get the text area's current text
-        var text = $(this)
-            .text()
-            .trim();
-        
-        // recreate the p element
-        var newEl = $("<p>")
-            .text(text);
-        
-        // replace the text input area with the p element
-        $(this).replaceWith(newEl);
-
-    });
-    
 };
+    
+var colorCode = function () {
 
+    var currentHour = new Date().getHours();
+
+    for (var i = 9; i <= 19; i++ ) {
+        
+        workDayHours[i] = i;
+
+
+    }
+    
+    // if hours are less than the current hour, background is gray
+    if ( workDayHours < currentHour) {
+
+    }
+
+
+    // if hours are equal to the current hour, background in red
+    else if ( hours === currentHour) {
+
+    }
+
+
+    // if hours are greater than current hour, background is green
+    else if (hours > currentHour ) {
+
+    }
+
+};
 
 var main = function () {
 
     displayCurrentDay();
 
     createText();
+
 };
 
 main();
